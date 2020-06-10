d6519c556154d5c400f73525192311c789396aa4
aylin <aylin.mara@hotmail.com>
2020-06-09 21:45:55 -0500

    session 14 de la ñaña renegona xd

---
+++ puchunguita/src/pages/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {LugarPage} from "../lugar/lugar";
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+   navegarALugar(){
+  	//alert("holiii");
+  	this.navCtrl.push(LugarPage);
+  }
+
+}
